01b07a625cb04606db2e6f68896551352973ee85
SaurabhK05 <saurabh.k150600@gmail.com>
2023-03-13 03:25:57 +0530

    Create Course Page

---
+++ src/components/Layouts/CoursePage/Course.js
@@ -0,0 +1,38 @@
+import React from "react";
+import SideBar from "../../Common/SideBar/SideBar";
+import TopBar from "../../Common/TopBar/TopBar";
+import Footer from "../../Common/Footer/Footer";
+import "./Course.css";
+import SubjectCard from "../../Common/SubjectCard/SubjectCard";
+
+export default function Course() {
+  return (
+    <>
+      <div className="mainContainer">
+        <SideBar />
+        <div className="mainMiddleContainer">
+          <div className="middleContainer">
+            <TopBar title="Courses" />
+            <div className="subjects">
+              <div className="subjectHeader">
+                <p>Topics</p>
+                <div className="subjectTab">
+                  <p className="activeSubjectTab">Recordings</p>
+                  <p>Assignments</p>
+                  <p>Notes</p>
+                </div>
+              </div>
+              <div className="subjectDetails">
+                <SubjectCard />
+                <SubjectCard />
+                <SubjectCard />
+                <SubjectCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
