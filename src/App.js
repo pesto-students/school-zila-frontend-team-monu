7eaf44f639db82f4191d5c32b4042fbb6d8073bd
mastery-vishal-kori <vishalkori1995@gmail.com>
2023-03-15 01:39:07 +0530

    added calander

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
 import Student from "./components/Student";
+import {Calander} from "./components/Calander";
+import SideBar from './components/SideBar';
 
 function App() {
-  return <Student />;
+  return (
+    <div style={{display:'flex'}}>
+    <SideBar />
+  <Calander />;
+    </div>
+  )
 }
 
-export default App;
+export default App; 
