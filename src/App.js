dca2828b71e9ec16d318c1adfd489d92dfe55470
SaurabhK05 <saurabhku@maqsoftware.com>
2023-03-14 14:56:34 +0530

    make footer sticky

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./App.css";
-import AnalysisLecture from "./components/Layouts/CoursePage/AnalysisLecture";
-// import Course from "./components/Layouts/CoursePage/Course";
+// import AnalysisLecture from "./components/Layouts/CoursePage/AnalysisLecture";
 // import Course from "./components/Layouts/CoursePage/Course";
 // import AddNewStudent from "./components/Layouts/StudentPage/AddNewStudent";
 // import Student from "./components/Layouts/StudentPage/Student";
 // import AddNewTeacher from "./components/Layouts/TeacherPage/AddNewTeacher";
-// import Teacher from "./components/Layouts/TeacherPage/Teacher";
+import Teacher from "./components/Layouts/TeacherPage/Teacher";
 
 function App() {
   // return <div>{false ? <Student /> : <Teacher />}</div>;
   return (
-    <div>
-      <AnalysisLecture />
+    <div className="rootDiv1">
+      <Teacher />
     </div>
   );
 }
