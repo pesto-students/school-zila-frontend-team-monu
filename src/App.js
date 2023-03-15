aba72c5186f4ece7f8687cdf9caa0faeb9f7539c
SaurabhK05 <saurabh.k150600@gmail.com>
2023-03-15 16:19:42 +0530

    fixed imports issues

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import LandingPage from "./components/Layouts/LandingPage/LandingPage";
 // import AddNewStudent from "./components/Layouts/StudentPage/AddNewStudent";
 // import Student from "./components/Layouts/StudentPage/Student";
 // import AddNewTeacher from "./components/Layouts/TeacherPage/AddNewTeacher";
-import Teacher from "./components/Layouts/TeacherPage/Teacher";
+// import Teacher from "./components/Layouts/TeacherPage/Teacher";
 
 function App() {
   // return <div>{false ? <Student /> : <Teacher />}</div>;
