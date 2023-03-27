4bf7b6f8efc1a14a77a5ebe1c7cbc6d0e7c71713
mastery-vishal-kori <vishalkori1995@gmail.com>
2023-03-27 22:46:40 +0530

    added prop

--- src/components/Common/Loader/Loader.js
+++ src/components/Common/Loader/Loader.js
@@ -1,10 +1,10 @@
 import React from "react";
 import CircularProgress from '@mui/material/CircularProgress';
 
-export default function Loader(){
+export default function Loader({size}){
     return (
         <div>
-            <CircularProgress />
+            <CircularProgress size={size} />
         </div>
     )
 }
