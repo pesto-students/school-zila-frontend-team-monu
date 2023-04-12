f2e8bf771151d7f1017adef3c013ecb6769284f8
SaurabhK05 <saurabh.k150600@gmail.com>
2023-04-13 01:50:15 +0530

    fix dashboard page

--- src/components/Layouts/Admin/Dashboard.js
+++ src/components/Layouts/Admin/Dashboard.js
@@ -136,116 +136,123 @@ export default function Dashboard() {
     { field: "amount", headerName: "Amount", width: 130 },
   ];
   return (
-    <div>
+    <div className="mainContainer">
       <SideBar />
-      <div style={{ marginLeft: "18vw" }}>
-        <TopBar title="Dashboard" />
-      </div>
-      <div className="dash-main">
-        <div className="dash-main-layout">
-          <div className="dash-main-top">
-            <div className="dash-sub-top">
-              <img src={StudentImg} alt="Student Image Icon" />
-              <span style={{ marginLeft: "10px" }}>
-                <div>students</div>
-                <span>1932</span>
-              </span>
-            </div>
-            <div className="dash-sub-top">
-              <img src={TeacherImg} alt="Teachers Image Icon" />
-              <span style={{ marginLeft: "10px" }}>
-                <div>Teachers</div>
-                <span>754</span>
-              </span>
-            </div>
-            <div className="dash-sub-top">
-              <img src={EventImg} alt="Event Image Icon" />
-              <span style={{ marginLeft: "10px" }}>
-                <div>Events</div>
-                <span>40</span>
-              </span>
-            </div>
-          </div>
-          <br />
-          <div className="dash-main-mid">
-            <LineChart
-              css={{
-                width: "125vh",
-                backgroundColor: "white",
-                borderRadius: "12px",
-              }}
-            />
-            <br />
-            <div id="calendar-mid">
-              <Calendar sx={{ borderRadius: "10px" }} />
-              {/* <Calendar onChange={onChange} value={value} /> */}
-              <BarChart
-                css={{
-                  width: "66vh",
+      <div className="mainMiddleContainer">
+        <div className="middleContainer">
+          <TopBar title="Dashboard" />
+          <div className="dashboard-container">
+            <div className="dash-main-layout">
+              <div className="dash-main-top">
+                <div className="dash-sub-top">
+                  <img src={StudentImg} alt="Student Image Icon" />
+                  <span style={{ marginLeft: "10px" }}>
+                    <div>students</div>
+                    <span>1932</span>
+                  </span>
+                </div>
+                <div className="dash-sub-top">
+                  <img src={TeacherImg} alt="Teachers Image Icon" />
+                  <span style={{ marginLeft: "10px" }}>
+                    <div>Teachers</div>
+                    <span>754</span>
+                  </span>
+                </div>
+                <div className="dash-sub-top">
+                  <img src={EventImg} alt="Event Image Icon" />
+                  <span style={{ marginLeft: "10px" }}>
+                    <div>Events</div>
+                    <span>40</span>
+                  </span>
+                </div>
+              </div>
+              <br />
+              <div className="dash-main-mid">
+                <LineChart
+                  css={{
+                    backgroundColor: "white",
+                    borderRadius: "12px",
+                  }}
+                />
+                <br />
+                <div id="calendar-mid">
+                  <Calendar sx={{ borderRadius: "10px", width: "47.5%" }} />
+                  {/* <Calendar onChange={onChange} value={value} /> */}
+                  <BarChart
+                    css={{
+                      width: "47.5%",
+                      backgroundColor: "white",
+                      borderRadius: "12px",
+                    }}
+                  />
+                </div>
+              </div>
+              <br />
+              <div
+                style={{
+                  height: "60vh",
                   backgroundColor: "white",
-                  borderRadius: "12px",
-                  marginLeft: "1vw",
+                  borderRadius: "10px",
                 }}
-              />
+              >
+                <div
+                  style={{
+                    fontSize: "18px",
+                    padding: "10px",
+                    color: "#303972",
+                  }}
+                >
+                  Unpaid Students Intution
+                </div>
+                <DataGrid
+                  rows={rows}
+                  columns={columns}
+                  pageSize={5}
+                  rowsPerPageOptions={[5]}
+                  sx={{
+                    color: "#303972",
+                    paddingLeft: "1vw",
+                    fontSize: "10px",
+                  }}
+                />
+              </div>
+            </div>
+            <div className="dash-main-right-column">
+              <div className="dash-main-right-column-messages">
+                <div className="messages-main">
+                  <div>Message</div>
+                  <div>{"You have 32 messages"}</div>
+                </div>
+                <img src={PlusIcon} alt="Plus Icon" />
+              </div>
+              <div>
+                {dummyMessage.map((i) => (
+                  <Message
+                    senderName={i.senderName}
+                    inClass={i.inClass}
+                    key={`${i.senderName}+${i.inClass}`}
+                  />
+                ))}
+              </div>
+              <button>View More</button>
+              <div className="dash-main-right-column-notifications">
+                <div className="notifications-main">
+                  <div>Notifications</div>
+                  <div>{"You have 32 notifications"}</div>
+                </div>
+              </div>
+              <div>
+                {dummyNotifications.map((i) => (
+                  <Notification
+                    senderName={i.senderName}
+                    info={i.info}
+                    key={`${i.senderName}+${i.info}`}
+                  />
+                ))}
+              </div>
+              <button>View More</button>
             </div>
           </div>
-          <br />
-          <div
-            style={{
-              height: "61vh",
-              width: "125vh",
-              backgroundColor: "white",
-              borderRadius: "10px",
-            }}
-          >
-            <div
-              style={{ fontSize: "18px", padding: "10px", color: "#303972" }}
-            >
-              Unpaid Students Intution
-            </div>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              pageSize={5}
-              rowsPerPageOptions={[5]}
-              sx={{ color: "#303972", paddingLeft: "1vw", fontSize: "10px" }}
-            />
-          </div>
-        </div>
-        <div className="dash-main-right-column">
-          <div className="dash-main-right-column-messages">
-            <div className="messages-main">
-              <div>Message</div>
-              <div>{"You have 32 messages"}</div>
-            </div>
-            <img src={PlusIcon} alt="Plus Icon" />
-          </div>
-          <div>
-            {dummyMessage.map((i) => (
-              <Message
-                senderName={i.senderName}
-                inClass={i.inClass}
-                key={`${i.senderName}+${i.inClass}`}
-              />
-            ))}
-          </div>
-          <button>View More</button>
-          <div className="dash-main-right-column-notifications">
-            <div className="notifications-main">
-              <div>Notifications</div>
-              <div>{"You have 32 notifications"}</div>
-            </div>
-          </div>
-          <div>
-            {dummyNotifications.map((i) => (
-              <Notification
-                senderName={i.senderName}
-                info={i.info}
-                key={`${i.senderName}+${i.info}`}
-              />
-            ))}
-          </div>
-          <button>View More</button>
         </div>
       </div>
     </div>
