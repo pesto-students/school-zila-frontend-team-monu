08c2feb1aedbd8755eacf6583a337902e05f874e
mastery-vishal-kori <vishalkori1995@gmail.com>
2023-04-25 11:12:43 +0530

    courses page corrections

--- src/components/Common/UploadFile/Upload.js
+++ src/components/Common/UploadFile/Upload.js
@@ -1,40 +1,205 @@
-import React from "react";
+import React, { useState } from "react";
 import UploadIcon from "../../../assets/upload-file.png";
+import serviceAxiosInstance from "../../../service/axiosService";
+import ToasterSnackbar from "../../Common/Toaster/toasterAlerts";
+import { TOASTER_STATUS } from "../../../../src/utils/constants";
+import { updateTosterStatus } from "../../../utils/commonService";
 import "./Upload.css";
 
+const dummyUpload = {
+  topicName: "",
+  lectureUrl: "",
+  lectureDetail: "",
+  teacherName: "",
+  rating: "",
+};
+const dummyAssignment = {
+  topicName: "",
+  assignmentUrl: "",
+  assignmentDetail: "",
+  teacherName: "",
+  rating: "",
+};
+
+let snackBarMessage = "";
 export default function UploadFile() {
+  const [openToaster, setOpenToaster] = useState(false);
+  const [alertStatus, setAlertStatus] = useState(null);
+  const [uploadLecture, setUploadLecture] = useState(dummyUpload);
+  const [uploadAssignment, setUploadAssignment] = useState(dummyAssignment);
+
+  const handleLectureChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event?.target;
+    setUploadLecture({ ...uploadLecture, [name]: value });
+  };
+
+  const handleAssignmentChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event?.target;
+    setUploadAssignment({ ...uploadAssignment, [name]: value });
+  };
+
+  const handleUploadLecture = async () => {
+    try {
+      let response = await serviceAxiosInstance({
+        // url of the api endpoint (can be changed)
+        url: "uploadLecture/",
+        method: "POST",
+        data: uploadLecture,
+      });
+      if (response?.status) {
+        snackBarMessage = response?.message;
+        updateTosterStatus(
+          setOpenToaster,
+          setAlertStatus,
+          TOASTER_STATUS.SUCCESS
+        );
+        setAddNewBtnClick(false);
+      } else {
+        snackBarMessage = response?.message;
+        updateTosterStatus(
+          setOpenToaster,
+          setAlertStatus,
+          TOASTER_STATUS.ERROR
+        );
+      }
+    } catch (e) {
+      snackBarMessage = response?.message;
+      updateTosterStatus(setOpenToaster, setAlertStatus, TOASTER_STATUS.ERROR);
+      console.log("Error");
+    }
+  };
+
+  const handleUploadAssignment = async () => {
+    try {
+      let response = await serviceAxiosInstance({
+        // url of the api endpoint (can be changed)
+        url: "uploadAssignment/",
+        method: "POST",
+        data: uploadAssignment,
+      });
+      if (response?.status) {
+        snackBarMessage = response?.message;
+        updateTosterStatus(
+          setOpenToaster,
+          setAlertStatus,
+          TOASTER_STATUS.SUCCESS
+        );
+        setAddNewBtnClick(false);
+      } else {
+        snackBarMessage = response?.message;
+        updateTosterStatus(
+          setOpenToaster,
+          setAlertStatus,
+          TOASTER_STATUS.ERROR
+        );
+      }
+    } catch (e) {
+      snackBarMessage = response?.message;
+      updateTosterStatus(setOpenToaster, setAlertStatus, TOASTER_STATUS.ERROR);
+      console.log("Error");
+    }
+  };
+
+  const uploadFile = () => {
+    handleUploadLecture();
+    handleUploadAssignment();
+  };
+
   return (
-    <div className="upload-container">
-      <div className="upload-lectures">
-        <img src={UploadIcon} alt="upload lecture image" />
-        <p className="upload-title">Upload Lecture</p>
-        <p className="upload-breif-detail">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius,
-          aperiam.
-        </p>
-        <div class="file-input">
-          <input type="file" id="file" class="file" />
-          <label for="file">
-            Upload
-            <p class="file-name"></p>
+    <>
+      <div className="upload-container">
+        <div className="upload-lectures">
+          <img src={UploadIcon} alt="upload lecture image" />
+          <p className="upload-title">Upload Lecture</p>
+          <div class="file-input">
+            <input
+              type="file"
+              id="file"
+              class="file"
+              name="lectureUrl"
+              onChange={handleLectureChange}
+            />
+            <label for="file">
+              Upload
+              <p class="file-name"></p>
+            </label>
+          </div>
+          <label for="topicName" className="topicLabel">
+            Topic title:
           </label>
-        </div>
-      </div>
-      <div className="upload-assignment">
-        <img src={UploadIcon} alt="upload lecture image" />
-        <p className="upload-title">Upload Assignment</p>
-        <p className="upload-breif-detail">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius,
-          aperiam.
-        </p>
-        <div class="file-input">
-          <input type="file" id="file" class="file" />
-          <label for="file">
-            Upload
-            <p class="file-name"></p>
+          <input
+            className="lectureInput"
+            type="text"
+            name="topicName"
+            value={uploadLecture.topicName}
+            onChange={handleLectureChange}
+          />{" "}
+          <br />
+          <label for="lectureDetail" className="topicLabel">
+            Description:
           </label>
+          <textarea
+            className="lectureInput"
+            type="text"
+            name="lectureDetail"
+            rows="4"
+            value={uploadLecture.lectureDetail}
+            onChange={handleLectureChange}
+          />
         </div>
+        <div className="upload-assignment">
+          <img src={UploadIcon} alt="upload lecture image" />
+          <p className="upload-title">Upload Assignment</p>
+          <div class="file-input">
+            <input type="file" id="file" class="file" name="" />
+            <label for="file">
+              Upload
+              <p class="file-name"></p>
+            </label>
+          </div>
+          <label for="topicName" className="topicLabel">
+            Topic title:
+          </label>
+          <input
+            type="text"
+            name="topicName"
+            value={uploadAssignment.topicName}
+            onChange={handleAssignmentChange}
+          />{" "}
+          <br />
+          <label for="assignmentDetail" className="topicLabel">
+            Description:
+          </label>
+          <textarea
+            className="lectureInput"
+            type="text"
+            name="assignmentDetail"
+            rows="4"
+            value={uploadAssignment.assignmentDetail}
+            onChange={handleAssignmentChange}
+          />
+        </div>
+        {openToaster && (
+          <ToasterSnackbar
+            status={alertStatus}
+            openToaster={openToaster}
+            message={"Message for Toast"}
+            setOpenToaster={setOpenToaster}
+            alertStatus={alertStatus}
+            setAlertStatus={setAlertStatus}
+          />
+        )}
       </div>
-    </div>
+      <button
+        type="button"
+        id="submitButton"
+        value={uploadLecture.topicName}
+        onClick={uploadFile}
+      >
+        Submit
+      </button>
+    </>
   );
 }
--- src/components/Layouts/CoursePage/AnalysisLecture.js
+++ src/components/Layouts/CoursePage/AnalysisLecture.js
@@ -1,27 +1,11 @@
 import React, { useState } from "react";
 import TopBar from "../../Common/TopBar/TopBar";
-import tempVideo from "../../../assets/mov_bbb.mp4";
+// import tempVideo from "../../../assets/mov_bbb.mp4";
 import "./AnalysisLecture.css";
 import LectureSideBar from "../../Common/LectureSideBar/LectureSideBar";
 
-let dummyDescription = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
-doloremque architecto dolore, rem labore minus commodi
-libero earum id, quas provident quidem. Quisquam, veritatis
-fuga magni soluta esse ad ducimus eligendi illo ratione
-aliquam, cum animi, corporis dignissimos numquam porro
-consequatur ipsum laudantium suscipit expedita doloremque!
-Corporis fuga inventore error.
-Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-Iste suscipit aliquam sed atque tempore, vero quisquam quis
-dicta aut, nobis magni qui, quas cupiditate in reprehenderit
-possimus? Consequatur culpa fuga reiciendis delectus saepe
-quod distinctio repellat odit aut sed voluptates commodi
-itaque, aspernatur magnam debitis nemo alias suscipit quidem
-deserunt!`;
-
-export default function AnalysisLecture(props) {
-  const { setOpenLecture } = props;
-  const [description, setdescription] = useState(dummyDescription);
+export default function AnalysisLecture({ props }) {
+  const { setOpenLecture, lectureUrl, lectureDetail } = props;
   return (
     <>
       <div className="mainContainer">
@@ -34,12 +18,12 @@ export default function AnalysisLecture(props) {
             <div className="analysisLectureDiv">
               <div className="analysisLecMainDiv">
                 <video width="99%" height="55%" controls>
-                  <source src={tempVideo} type="video/mp4"></source>
+                  <source src={lectureUrl} type="video/mp4"></source>
                 </video>
                 <div className="lectureDetail">
                   <p className="lectureDetailHeader">Lecture Detail</p>
-                  <p>{description}</p>
-                </div> 
+                  <p>{lectureDetail}</p>
+                </div>
               </div>
               <LectureSideBar />
             </div>
