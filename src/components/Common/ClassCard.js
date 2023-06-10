2b5d8701cd260f06e3e2886061867ad3ef5007ab
SaurabhK05 <saurabh.k150600@gmail.com>
2023-06-10 13:11:54 +0530

    add subject page

--- src/components/Common/ClassCard.js
+++ src/components/Common/ClassCard.js
@@ -1,29 +1,63 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
-import { CardActionArea } from "@mui/material";
-// import ClassIcon from "../../assets/Class Icons-03.png";
+import { CardActionArea, Menu, MenuItem } from "@mui/material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
-export default function ActionAreaCard({ ClassIcon }) {
+export default function ActionAreaCard({ ClassIcon, SubjectIcon }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div style={{ padding: "2% 4% 2% 4%" }}>
-      <Card
-        sx={{
-          width: 150,
-          height: 150,
-        }}
-      >
-        <CardActionArea style={{ height: "inherit" }}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        <div style={{ padding: "2% 4% 2% 4%" }}>
+          <Card
+            sx={{
+              width: 150,
+              height: 150,
             }}
           >
-            <img src={ClassIcon} alt="" style={{ width: "65%" }} />
-          </div>
-        </CardActionArea>
-      </Card>
+            <CardActionArea style={{ height: "inherit" }}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img src={ClassIcon} alt="" style={{ width: "65%" }} />
+              </div>
+            </CardActionArea>
+          </Card>
+        </div>
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Subjects"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {SubjectIcon.map((el) => {
+              return <img src={el} alt="" />;
+            })}
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
--- src/components/Layouts/ClassesPage/ClassPage.js
+++ src/components/Layouts/ClassesPage/ClassPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+
 import TopBar from "../../Common/TopBar/TopBar";
 import ActionAreaCard from "../../Common/ClassCard";
 import class1 from "../../../assets/class1.png";
@@ -13,56 +14,113 @@ import class9 from "../../../assets/Class Icons-09.png";
 import class10 from "../../../assets/class110.png";
 import class11 from "../../../assets/class11.png";
 import class12 from "../../../assets/class12.png";
+import English from "../../../assets/englishIcon.webp";
+import Hindi from "../../../assets/HindiIcon.webp";
+import Accounts from "../../../assets/accountancyIcon.webp";
+import Biology from "../../../assets/biologyIcon.webp";
+import Business from "../../../assets/business-studiesIcon.webp";
+import Chemistry from "../../../assets/chemistryIcon.webp";
+import Computer from "../../../assets/computerIcon.webp";
+import Evs from "../../../assets/evsIcons.webp";
+import Geography from "../../../assets/geographyIcon.png";
+import GK from "../../../assets/gkIcon.webp";
+import Maths from "../../../assets/mathsIcon.webp";
+import PhyscialEDU from "../../../assets/physical-educationIcon.webp";
+import Physics from "../../../assets/physicsIcon.webp";
+import PoliticalS from "../../../assets/political-scienceIcons.webp";
+import Psychology from "../../../assets/psychologyIcon.png";
+import Sanskrit from "../../../assets/sanskritIcon.webp";
+import Science from "../../../assets/scienceIcon.webp";
+import SocialS from "../../../assets/social-scienceIcon.webp";
+import Sociology from "../../../assets/sociologyIcon.png";
+import Economics from "../../../assets/economicsIcon.webp";
 
 export default function ClassPage({ setShowSideBar }) {
   const classes = [
     {
       class: "1",
       asset: class1,
+      subject: [Hindi, English, Evs, Maths, GK, Computer],
     },
     {
       class: "2",
       asset: class2,
+      subject: [Hindi, English, Evs, Maths, GK, Computer],
     },
     {
-      class: "1",
+      class: "3",
       asset: class3,
+      subject: [Hindi, English, Evs, Maths, GK, Computer],
     },
     {
-      class: "1",
+      class: "4",
       asset: class4,
+      subject: [Hindi, English, Evs, Maths, GK, Computer],
     },
     {
-      class: "1",
+      class: "5",
       asset: class5,
+      subject: [Hindi, English, Evs, Maths, GK, Computer],
     },
     {
-      class: "1",
+      class: "6",
       asset: class6,
+      subject: [Hindi, English, Science, Maths, SocialS, Sanskrit, Computer],
     },
     {
-      class: "1",
+      class: "7",
       asset: class7,
+      subject: [Hindi, English, Science, Maths, SocialS, Sanskrit, Computer],
     },
     {
-      class: "1",
+      class: "8",
       asset: class8,
+      subject: [Hindi, English, Science, Maths, SocialS, Sanskrit, Computer],
     },
     {
-      class: "1",
+      class: "9",
       asset: class9,
+      subject: [Hindi, English, Science, Maths, SocialS, Sanskrit],
     },
     {
-      class: "1",
+      class: "10",
       asset: class10,
+      subject: [Hindi, English, Science, Maths, SocialS, Sanskrit],
     },
     {
-      class: "1",
+      class: "11",
       asset: class11,
+      subject: [
+        Maths,
+        Biology,
+        Physics,
+        Chemistry,
+        Business,
+        PhyscialEDU,
+        PoliticalS,
+        Sociology,
+        Psychology,
+        English,
+      ],
     },
     {
-      class: "1",
+      class: "12",
       asset: class12,
+      subject: [
+        Accounts,
+        Biology,
+        Physics,
+        Chemistry,
+        Economics,
+        Business,
+        PhyscialEDU,
+        PoliticalS,
+        Sociology,
+        Psychology,
+        English,
+        Geography,
+        Maths,
+      ],
     },
   ];
 
@@ -82,7 +140,12 @@ export default function ClassPage({ setShowSideBar }) {
                 </div>
                 <div style={{ display: "flex", flexWrap: "wrap" }}>
                   {classes.map((el) => {
-                    return <ActionAreaCard ClassIcon={el.asset} />;
+                    return (
+                      <ActionAreaCard
+                        ClassIcon={el.asset}
+                        SubjectIcon={el.subject}
+                      />
+                    );
                   })}
                 </div>
               </div>
